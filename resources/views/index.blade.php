c6207b145643c242ea9ca7c1c6f83a4633ee11de
Claudio Rivera Bravo <riverabravocl@outlook.com>
2024-01-04 23:06:40 -0300

    Mejorando el aspecto del sitio con Tailwind CSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,17 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Laravel</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
-    @foreach ($posts as $post)
-        <div>
-            <h2>{{ $post->title }}</h2>
+<body class="bg-gray-100 text-gray-700">
+    <div class="container mx-auto px-4">
+        <div class="grid grid-cols-3 my-3">
+            @foreach ($posts as $post)
+                <div class="bg-white hover:bg-blue-100 border border-gray-200 p-5">
+                    <h2 class="font-bold text-lg mb-4">{{ $post->title }}</h2>
+                    <p class="text-xs">{{ $post->excerpt }}</p>
+                    <p class="text-xs text-right">{{ $post->created_at->format('d/m/Y') }}</p>
+                </div>
+            @endforeach
         </div>
-    @endforeach
-    {{ $posts->links() }}
+        <div class="mb-10">
+            {{ $posts->links() }}
+        </div>
+    </div>
 
 </body>
 
