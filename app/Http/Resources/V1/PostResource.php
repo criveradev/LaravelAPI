a1468abf6b57e9deceb8ba4361188f7c68ee9e2d
Claudio Rivera Bravo <riverabravocl@outlook.com>
2024-01-05 14:15:49 -0300

    Versión 1: recurso

---
+++ app/Http/Resources/V1/PostResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        #Configuramos los recursos que deseamos mostrar en la api
+        return [
+            'title'   => $this->title,
+            'slug'    => $this->slug,
+            'excerpt' => $this->excerpt,
+            'content' => $this->content
+
+        ];
+    }
+}
